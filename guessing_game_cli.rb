d86ea50136789b308aeaf48524f579833bb9b0c5
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-02 02:31:04 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+def run_guessing_game
+    num_gen = rand(6)
+    puts "Please enter your number: "
+    user_input = gets.chomp
+    if user_input == num_gen
+      puts "You guessed the correct number!"
+      elsif user_input != num_gen
+         puts "Sorry! The computer guessed #{num_gen}"
+       else
+         user_input == "exit"
+    end
+   puts '/Goodbye!/'
+end
+  run_guessing_game
